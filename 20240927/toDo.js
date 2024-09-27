172a9a8786953233957916578fca5dc7294aab05
Meija Lohiniva <meija.lohiniva@oamk.fi>
2024-09-27 09:40:18 +0300

    pseudo code for todo

---
+++ 20240927/toDo.js
@@ -0,0 +1,22 @@
+// Create array for tasks
+// Print menu
+// Read user input
+    // If input 1
+        // Ask name for task
+        // Add task into an array
+        // Print out "task added"
+        // print menu
+    // If input 2
+        // loop through task array
+            // print task name
+        // print menu
+    // If input 3
+        // ask for removed task index
+        // remove task from array
+        // print out "task removed"
+        // print menu
+    // if input 0
+        // end program
+    // else
+        // print out "invalid task"
+        // print menu
